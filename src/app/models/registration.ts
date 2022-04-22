2e54de2ff445fb4831b17ce05c89dd0928ef1817
GediminasGricius <g.gricius@ituostas.lt>
2022-04-22 19:06:28 +0300

    add new record

---
+++ src/app/models/registration.ts
@@ -0,0 +1,11 @@
+export class Registration{
+    constructor (
+        public mark:String, 
+        public model:String,
+        public reg_number:String, 
+        public phone:String, 
+        public year:number
+    ){
+        
+    }
+}
