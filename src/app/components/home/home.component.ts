7ee281eeda6d69c63d0748775e9c3a238a5e0aa4
GediminasGricius <g.gricius@ituostas.lt>
2022-04-27 19:42:10 +0300

    Login

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthResponseData } from 'src/app/models/authResponseData';
 import { Registration } from 'src/app/models/registration';
+import { AuthService } from 'src/app/services/auth.service';
 import { RegistrationService } from 'src/app/services/registration.service';
 
 @Component({
@@ -11,8 +13,10 @@ import { RegistrationService } from 'src/app/services/registration.service';
 export class HomeComponent implements OnInit {
 
   public registrations:Registration[]=[];
+  public isLoggedin=false;
+  public user?:AuthResponseData;
 
-  constructor(private registrationService:RegistrationService) { }
+  constructor(private registrationService:RegistrationService, private auth:AuthService, private router:Router) { }
 
   private loadData(){
     this.registrationService.getRegistrations().subscribe((response)=>{
@@ -22,12 +26,21 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadData();
+    this.isLoggedin=this.auth.isLoggedIn;
+    this.user=this.auth.user;
   }
 
   deleteRegistration(id:String){
     this.registrationService.deleteRegistration(id).subscribe((response)=>{
       this.loadData();
     });
+  }
+
+  onLogout(){
+    this.auth.logout();
+    this.router.navigate(['/login']);
+
+
   }
 
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { AuthResponseData } from '../models/authResponseData';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public isLoggedIn=false;
+  public user?:AuthResponseData;
 
   constructor(private http:HttpClient) { }
 
@@ -14,6 +17,26 @@ export class AuthService {
       email:email,
       password:password,
       returnSecureToken:true
-    });
+    }).pipe(  tap(  (response)=>{
+      this.isLoggedIn=true;
+      this.user=response;
+    }));
   }
+
+  public login(email:String,password:String){
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCt63eSHlVde6Jqubnf7IIkv-zwVdzD9xQ',{
+      email:email,
+      password:password,
+      returnSecureToken:true
+    }).pipe(  tap(  (response)=>{
+      this.isLoggedIn=true;
+      this.user=response;
+    }));
+  }
+
+  public logout(){
+    this.isLoggedIn=false;
+    this.user=undefined;
+  }
+
 }
