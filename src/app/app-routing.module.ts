d9c91b5eb90291a3a9edc64b6b8353992001bb11
GediminasGricius <g.gricius@ituostas.lt>
2022-05-02 19:04:01 +0300

    reactive forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { HomeComponent } from './components/home/home.component';
 import { NewRegistrationComponent } from './components/new-registration/new-registration.component';
+import { NewTechnicianComponent } from './components/new-technician/new-technician.component';
 import { UpdateRegistrationComponent } from './components/update-registration/update-registration.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'new', component:NewRegistrationComponent},
   {path: 'update/:id', component:UpdateRegistrationComponent},
-  {path: 'login', component:AuthComponent}
+  {path: 'login', component:AuthComponent},
+  {path: 'newTechnician', component:NewTechnicianComponent}
 ];
 
 @NgModule({
